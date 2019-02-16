c52c8041568d1b043ffaca4bb618dd2158127e68
Michael Miranda <Michael@michaels-mbp.lan>
2019-02-16 14:32:35 -0500

    Version 1

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop"
+import classes from "./SideDrawer.css"
+
+const SideDrawer = props => {
+  let attachClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachClasses = [classes.SideDrawer, classes.Open]
+  }
+  return (
+    <Fragment>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Fragment>
+  )
+}
+
+
+export default SideDrawer;
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from "react";
+import Backdrop from "../Backdrop/Backdrop";
+import classes from "./Modal.css";
+
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  componentWillUpdate() {
+    console.log("Modal will update")
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div 
+          className={classes.Modal}
+          style= {{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default Modal;
