a75021fc39d9a8c5e1f86951f6607babd3c2153c
Michael Miranda <42720791+mrpineapples@users.noreply.github.com>
2019-03-04 16:04:31 -0500

    Refactor & add lazy loading

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,28 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import Button from "../../UI/Button/Button"
 
-class OrderSummary extends Component {
-  // this can be a fucntional component as well
-
-  componentWillUpdate() {
-    console.log("[OrderSummary] will update");
-  }
-
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}
-        </li>  
-      )
-    })
-
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
     return (
-      <Fragment>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
-      </Fragment>
+      <li key={igKey}>
+        <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
+      </li>  
     )
-  }
+  })
+
+  return (
+    <Fragment>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+    </Fragment>
+  )
 }
 
 export default OrderSummary;
